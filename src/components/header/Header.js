4046e43d144b6026e4a933a3c47335df40dd0671
Mario <mariob360@gmail.com>
2020-04-28 19:56:47 -0700

    search box added to react controlled component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,12 +9,16 @@ const Header = (props) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [toggleSearch, setToggleSearch] = useState(true);
 
+  const checkEmpty = (str) => {
+    return (!str || /^\s*$/.test(str));
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(searchTerm);
+    if (checkEmpty(searchTerm)) return;
 
-    // todo : check if valid location before clearing
+    // TODO : send back search query to wrapping container state
+    // TODO : check if valid location before clearing
     setSearchTerm('')
   }
 
@@ -22,54 +26,44 @@ const Header = (props) => {
     setSearchTerm(e.target.value)
   }
 
-
-  // FIX TOGGLE OF SEARCH
-
-  const BLURRING = (e) => {
-    alert('BLURRRRRRING')
-  }
-
-  useEffect(() => {
-    effect
-    return () => {
-      cleanup
-    }
-  }, [])
-
   return (
     <header className='header-container'>
 
       <h1 className='location'>{location}</h1>
 
+      {/* SEPARATE COMPONENT LATER :) */}
       <div className='searchContainer'>
-
         <form className='search' id='search' onSubmit={handleSubmit}>
 
           <input
-            onFocus={() => setToggleSearch(false)}
-            onBlur={(e) => BLURRING(e)}
+            onFocus={() =>  setToggleSearch(false)}
+            onBlur={() => setToggleSearch(true)}
+            onChange={handleChange}
+
             type="text"
             placeholder='Search'
+
             name='inputSearch'
             id='inputSearch'
             className='inputSearch'
+
             value={searchTerm}
-            onChange={handleChange}
             autoComplete='off'
           />
 
-          {toggleSearch ?
+
+          {(!toggleSearch || !checkEmpty(searchTerm)) &&
+            <label htmlFor="inputSubmit" className={'submitIcon'}>
+              <GoSearch color={'rgb(159, 159, 159)'} size={45} className={'goSearch'} />
+              <input onClick={handleSubmit} type="submit" name='inputSubmit' id='inputSubmit' />
+            </label>
+            }
+
+          {(toggleSearch || checkEmpty(searchTerm)) &&
             <label htmlFor='inputSearch' className='searchIcon'>
               <GoSearch color={'rgb(187, 187, 187)'} size={45} className={'goSearch'} />
             </label>
-            :
-            <label htmlFor="inputSubmit" className='searchIcon'>
-              {/* <GoSearch color={'rgb(139, 139, 139)'} size={45} className={'goSearch'} /> */}
-              <GoSearch color={'red'} size={45} className={'goSearch'} />
-              <input type="submit" value='' name='inputSubmit' id='inputSubmit' />
-            </label>
           }
-
 
         </form>
       </div>
