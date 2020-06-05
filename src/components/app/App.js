a0380a1563b9d2b900424fdcac1edbb87089a92a
Mario <mariob360@gmail.com>
2020-06-04 17:01:03 -0700

    Fetch current, hourly, weekly data from API Set all data to state

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,13 +3,14 @@ import './App.css';
 
 // Components
 import Header from '../header/Header';
-import CurrentWeather from '../currentWeather/CurrentWeather';
+import CurrentTemp from '../currentTemp/CurrentTemp';
 
 
 function App() {
 
   const [ location, setLocation ] = useState('Fresno');
   const [ currentWeatherData, setCurrentWeatherData ] = useState();
+  const [ hourlyWeatherData, setHourlyWeatherData ] = useState();
   const [ weeklyWeatherData, setWeeklyWeatherData ] = useState();
 
   function timeConverter(UNIX_timestamp){
@@ -28,52 +29,36 @@ function App() {
     return time;
   }
 
-  // Daily / Weekly Data
+  // Daily | Weekly | Hourly Weather Data
   useEffect(() => {
     const fetchDailyData = async () => {
 
+      // ONLY FOR THE LAT, LON, LOCATION NAME
       const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`);
       const data = await res.json();
-
-      const { lat, lon } = data.coord;
-      const resOneCall = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,current&units=imperial&appid=${process.env.REACT_APP_API_KEY}`);
-      const resOneCallData = await resOneCall.json();
-
-      setCurrentWeatherData(data);
-      setWeeklyWeatherData(resOneCallData);
-
-      // Change Location to api name
       setLocation(data.name)
 
+      const { lat, lon } = data.coord;
+      const resOneCall = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=imperial&appid=${process.env.REACT_APP_API_KEY}`);
+      const resOneCallData = await resOneCall.json();
 
-      console.log(`current`);
-      console.log(data);
-
-
-      console.log(`resOneCallData`);
-      console.log(resOneCallData);
-
+      const {current, daily, hourly} = resOneCallData;
+      // change hourly to only 24hrs
+      setCurrentWeatherData(current);
+      setHourlyWeatherData(hourly);
+      setWeeklyWeatherData(daily);
     }
 
     fetchDailyData();
 
   }, [location]);
 
-  // Check for current data
-  useEffect(() => {
-    if(setCurrentWeatherData) {
-      const weather = setCurrentWeatherData.main;
-      const time = timeConverter(setCurrentWeatherData.dt)
-
-    }
-  }, [setCurrentWeatherData])
-
   // SET LOADING FOR THE COMPONENTS WHILE THE DATA IS BEING FETCHED
 
   return (
-    <div>
+    <div className="app-container">
       <Header location={location}  setLocation={setLocation} />
-      <CurrentWeather currentWeatherData={currentWeatherData}/>
+      <CurrentTemp currentWeatherData={currentWeatherData}/>
     </div>
   );
 }
